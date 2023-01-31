cba7e86035cfea67bb20f45900d8abebf39c7e5b
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-01-31 13:53:34 +0200

    Created project basic files

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Installed basic packages
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql'
+})
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to joga_mysql database successfully!')
+})
+
+app.listen(3000, () => {
+    console.log('App is started at https://localhost:3000')
+})
